ed0a28699f0556893afcef6b74966dec612873b9
rpsingh6634 <rpsingh6634@gmail.com>
2022-05-29 17:46:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Slider from './Components/Slider/Slider'
+import Navbar from './Components/Navbar/Navbar'
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Slider />
+      
+    </>
+  );
+}
+
+export default App;
